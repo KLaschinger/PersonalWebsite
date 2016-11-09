ba11e5ed67e65cdc0a04530dc8fab1ab4d4bf8a9
Kurt Laschinger <kurtlasc@gmail.com>
2016-11-09 18:00:19 -0500

    Finished an inital js website background.  Not what I wanted exactly, but it looks cool anyways

--- public/javascripts/background.js
+++ public/javascripts/background.js
@@ -11,11 +11,22 @@ function initialize() {
 
   for (i = 0; i < widthCount; i++) {
     for (j = 0; j < heightCount; j++) {
+      var factor = Math.floor(Math.random() * 150);
+      var rginit = 0;
+      var binit = 255;
+
+      if (factor > 75) {
+        rginit += (factor - 100);
+      } else {
+        binit -= factor;
+      }
+
       var obj = {
-        rect: new Rectangle(i, j, i+SQUARE_SIZE, j+SQUARE_SIZE),
-        direction: 'up', // True is up and false is down
-        rg: 0,
-        b: 255
+        path: new Path.Rectangle(new Rectangle(i * SQUARE_SIZE, j * SQUARE_SIZE, (i + 1) * SQUARE_SIZE, (j + 1) * SQUARE_SIZE)),
+        height: j,
+        direction: true, // True is up and false is down
+        rg: rginit,
+        b: binit
       };
 
       squares.push(obj);
@@ -26,61 +37,44 @@ function initialize() {
 
 function onFrame(event) {
   for (i = 0; i < squares.length; ++i) {
-
-    if (squares[i].direction == 'up') {
-      if (!(squares[i].rg > 0)) {
+    if (squares[i].direction == true) {
+      if (squares[i].rg == 0) {
         if (squares[i].b < 255-75) {
-          if (squares[i].direction == 'up') {
-            squares[i].direction = 'down';
-          } else  {
-            squares[i].direction == 'up';
-          }
+          squares[i].direction = false;
         } else {
-          if (Math.floor(Math.random() * (255 - squares[i].b) * (squares[i].rect.height/2)) > 50) {
-            if (squares[i].direction == 'up') {
-              squares[i].direction = 'down';
-            } else  {
-              squares[i].direction == 'up';
-            }
+          if (Math.floor(Math.random() * (255 - squares[i].b) + squares[i].height) > 60) {
+            squares[i].direction = false;
           }
         }
       }
     } else {
-      if (!(squares[i].b < 255)) {
+      if (squares[i].b == 255) {
         if (squares[i].rg > 75) {
-          if (squares[i].direction == 'up') {
-            squares[i].direction = 'down';
-          } else  {
-            squares[i].direction == 'up';
-          }
+          squares[i].direction = true;
         } else {
-          if (Math.floor(Math.random() * squares[i].rg * (squares[i].rect.height/2)) > 50) {
-            if (squares[i].direction == 'up') {
-              squares[i].direction = 'down';
-            } else  {
-              squares[i].direction == 'up';
-            }
+          if (Math.floor(Math.random() * squares[i].rg + squares[i].height) > 60) {
+            squares[i].direction = true;
           }
         }
       }
     }
 
-    if (squares[i].direction == 'up') {
-      if (squares[i].b < 255) {
-        squares[i].b += 1;
+    if (squares[i].direction == true) {
+      if (squares[i].rg == 0) {
+        squares[i].b -= 1;
       } else {
         squares[i].rg -= 1;
       }
     } else {
-      if (squares[i].rg > 0) {
+      if (squares[i].b == 255) {
         squares[i].rg += 1;
       } else {
-        squares[i].b -= 1;
+        squares[i].b += 1;
       }
     }
 
-    squares[i].rect.fillColor = new Color(squares[i].rg/255, squares[i].rg/255, squares[i].b/255);
-    console.log(squares[i].rg + " " + squares[i].rg + " " + squares[i].b);
+    squares[i].path.fillColor = new Color(squares[i].rg/255, squares[i].rg/255, squares[i].b/255);
+    //console.log(squares[i].rg + " " + squares[i].rg + " " + squares[i].b + " " + squares[i].direction);
   }
 }
 
